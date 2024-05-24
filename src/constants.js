0d45c7c5b544278badbbdb0dbd660916a25b2c6d
deepeshjaindj <jaind1616@gmail.com>
2024-05-25 00:56:22 +0530

    Added User and Video Model, Setup jwt and bcrypt

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,8 @@ export const DB_NAME = 'videovox';
 export const MONGODB_URI = process.env.MONGODB_URI;
 export const PORT = process.env.PORT;
 export const CORS_ORIGIN = process.env.CORS_ORIGIN;
+
+export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
+export const ACCESS_TOKEN_EXPIRY = process.env.ACCESS_TOKEN_EXPIRY;
+export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
+export const REFRESH_TOKEN_EXPIRY = process.env.REFRESH_TOKEN_EXPIRY;
---
+++ src/models/user.model.js
@@ -0,0 +1,107 @@
+import mongoose, { Schema } from 'mongoose';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import { ACCESS_TOKEN_EXPIRY, ACCESS_TOKEN_SECRET, REFRESH_TOKEN_EXPIRY, REFRESH_TOKEN_SECRET } from '../constants';
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    }, 
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Video'
+      }
+    ],
+    password: {
+      type: String,
+      required: [true, 'Password is required']
+    },
+    refreshToken: {
+      type: String
+    }
+  },
+  {
+    timestamps: true,
+  }
+)
+
+/**
+ * Middleware function to hash the user's password before saving it to the database.
+ * It checks if the password field has been modified before hashing it.
+ */
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+})
+
+
+userSchema.methods.isPasswordCorrect = async function(password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+/**
+ * Method to generate an access token using JWT for the user.
+ * 
+ * @returns {string} Access token for the user with specified expiry.
+ */
+userSchema.methods.getAccessToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname
+    },
+    ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: ACCESS_TOKEN_EXPIRY
+    }
+  )
+}
+
+/**
+ * Method to generate a refresh token using the user's _id with specified expiry time and secret.
+ * @returns {string} The generated refresh token.
+ */
+userSchema.methods.getRefreshToken = function() {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: REFRESH_TOKEN_EXPIRY
+    }
+  )
+}
+
+export const User = mongoose.model('User', userSchema);
